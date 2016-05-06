9c3ae2597c71eb2c99517a5b52c414adbaff5fef
Luca Quatela <lucaqq@gmail.com>
2016-05-06 11:37:42 -0700

    add the list of stylests in index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,44 @@
 import java.util.HashMap;
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-
   public static void main (String[] args){
 
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist newStylist = new Stylist (request.queryParams("stylistname"));
+      newStylist.save();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    // get("/stylists", (request,response) -> {
+    //   HashMap<String, object> = new HashMap<String, object>();
+    //   model.put("stylists", Stylist.all());
+    //   model.put("template", "templates/stylists.vtl")
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+
+
+
   }
+
+
+
+
+
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,8 @@
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,12 +15,28 @@ public class AppTest extends FluentTest {
     return webDriver;
   }
 
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("");
+  @Test public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Add stylist");
+  }
+
+  @Test public void stylistIsCreated() {
+    goTo("http://localhost:4567/");
+    fill("#stylistname").with("Mark");
+    submit(".btn");
+    assertThat(pageSource()).contains("Mark");
+  }
+
+
+
+  // @Test public void stylistsListIsDisplayedCorrectly() {
+  //   goTo("http://localhost:4567/stylists");
   // }
 
 }
